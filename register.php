9ae208c4e48522e009490f0038c15ebfa146df20
kaikaine <kairijb@protonmail.com>
2019-02-22 06:39:55 -0600

    added php variables for user data and some validation for email

--- register.php
+++ register.php
@@ -1,3 +1,53 @@
+<?php
+$con = mysqli_connect('localhost', 'root', '', 'social');
+if(mysqli_connect_error()) {
+    echo 'Failed to connect: ' . mysqli_connect_error();
+}
+$fname = '';
+$lname = '';
+$email = '';
+$email2 = '';
+$password = '';
+$password2 = '';
+$date = '';
+$error_array = '';
+
+if(isset($_POST['register_button'])) {
+    $fname = strip_tags($_POST['reg_fname']); // remove html tags
+    $fname = str_replace(' ', '', $fname); // remove spaces
+    $fname = ucfirst(strtolower($fname));
+
+    $lname = strip_tags($_POST['reg_lname']); // remove html tags
+    $lname = str_replace(' ', '', $lname); // remove spaces
+    $lname = ucfirst(strtolower($lname));
+
+    $email = strip_tags($_POST['reg_email']); // remove html tags
+    $email = str_replace(' ', '', $email); // remove spaces
+    $email = ucfirst(strtolower($email));
+
+    $email2 = strip_tags($_POST['reg_email2']); // remove html tags
+    $email2 = str_replace(' ', '', $email2); // remove spaces
+    $email2 = ucfirst(strtolower($email2));
+
+    $password = strip_tags($_POST['reg_password']); // remove html tags
+    
+
+    $password2 = strip_tags($_POST['reg_password2']); // remove html tags
+
+    $date = date("Y-m-d");
+    
+    if($email == $email2) {
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        } else {
+            echo "Invalid Format";
+        }
+    } else {
+        echo "Emails don't match";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
